df0ebc31516f808c9a34d7922fcd4b09f3a54b64
Maksim Grushin <grusha-as@yandex.ru>
2024-01-22 19:04:15 +0500

    Added project Bank. Version 1.0.0

---
+++ Bank/Bank.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace Bank
+{
+    class Contribution
+    {
+        private double[] contributions;
+        private double[] finalyContribution;
+
+        public Contribution(double[] contributions)
+        {
+            this.contributions = contributions;
+            finalyContribution = new double[contributions.Length];
+        }
+
+        ~Contribution() 
+        {
+            Console.WriteLine("Вклад/вклады были очистены");
+        }
+
+        public void SetFinalyContribution(double[] contributs)
+        {
+            for(int i = 0; i < contributs.Length; i++)
+            {
+                finalyContribution[i] = contributs[i];
+            }
+        }
+
+        public int GetSizeContributions
+        {
+            get { return contributions.Length; }
+        }
+
+        public double[] GetContribution
+        {
+            get
+            {
+                return contributions;
+            }
+        }
+
+        public double[] GetFinalyContribution
+        {
+            get
+            {
+                return finalyContribution;
+            }
+        }
+    }
+
+    class Bank
+    {
+        private double percent = 0;
+        private double year = 0;
+        private double contribution = 0;
+        private Contribution contribut;
+
+        public Bank(Contribution contribut, double year = 1.5, double percent = 7.5) 
+        {
+            this.contribut = contribut;
+            this.year = year;
+            this.percent = percent;
+        }
+
+        public void CalculationDeposit()
+        {
+            FutureContribution();
+            DepositWithdrawal();
+        }
+
+        private void DepositWithdrawal()
+        {
+            Console.WriteLine("Вклад(ы): ");
+            for (int i = 0; i < contribut.GetSizeContributions; ++i)
+            {
+                Console.WriteLine($"{i + 1}. {contribut.GetContribution[i]}");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine($"На {year} лет, с процентом {percent}");
+            Console.WriteLine();
+
+            Console.WriteLine("Вышло: ");
+            for (int i = 0; i < contribut.GetSizeContributions; ++i)
+            {
+                Console.WriteLine($"{i + 1}. {contribut.GetFinalyContribution[i]}");
+            }
+
+        }
+
+        private void FutureContribution()
+        {
+            double totalPercent = percent * year;
+            double[] newContribut = new double[contribut.GetSizeContributions];
+            
+            for(int i = 0; i < contribut.GetSizeContributions; ++i)
+            {
+                newContribut[i] = contribut.GetContribution[i] * (totalPercent / 100 + 1);
+            }
+
+            contribut.SetFinalyContribution(newContribut);
+        }
+    }
+}
---
+++ Bank/Client.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace Person
+{
+    class Person
+    {
+        public string firstName = "";
+        public string secondName = "";
+        public int age;
+
+        private double money = 0;
+
+        protected Person(string firstName, string secondName, int age, double money = 10000)
+        {
+            this.firstName = firstName;
+            this.secondName = secondName;
+            this.age = age;
+            this.money = money;
+        }
+
+        public virtual double[] CreateContribut()
+        {
+            int numberContributs = 0;
+
+            Console.WriteLine("Меню создания вклада");
+            Console.Write("Введите количество вкладов: ");
+            numberContributs = int.Parse(Console.ReadLine());
+
+            while (numberContributs <= 0)
+            {
+                Console.WriteLine($"Введеное количество равно {numberContributs}\nЭто не допустимо! Введите новое количество вкладов превыщающий нуля");
+                numberContributs = int.Parse(Console.ReadLine());
+            }
+
+            double[] contributs = new double[numberContributs];
+
+            Console.WriteLine($"Кошелек: {money}");
+            if (money == 0)
+            {
+                Console.WriteLine("У вас недостаточно денег чтобы сделать вклад");
+            }
+            else
+            {
+                for (int i = 0; i < numberContributs; i++)
+                {
+                    Console.Write($"Введите количество денег для {i + 1}: ");
+                    double contribut = double.Parse(Console.ReadLine());
+                    double balance = money -= contribut;
+                    if (balance < 0)
+                    {
+                        while (balance < 0)
+                        {
+                            balance = 0;
+                            Console.WriteLine($"Невозможно выполнить, так как у вас {money} денег");
+                            Console.Write($"Введите количество денег для {i + 1}: ");
+                            contribut = double.Parse(Console.ReadLine());
+                            balance = money -= contribut;
+                        }
+                    }
+                    else if (balance == 0)
+                    {
+                        money -= contribut;
+                        contributs[i] = contribut;
+                        Console.WriteLine($"Кошелек: {money}");
+                        Console.WriteLine("Закончились деньги, производится выход...");
+                        break;
+                    }
+
+                    money -= contribut;
+                    contributs[i] = contribut;
+                }
+            }
+
+            return contributs;
+        }
+    }
+
+    class Client : Person
+    {
+        public Client(string firstName, string secondName, int age, double money = 10000) : base(firstName, secondName, age, money)
+        {}
+
+        ~Client() 
+        {
+            Console.WriteLine($"Персонаж с именем {firstName} и фамилием {secondName}, стёрт");
+        }
+    }
+
+    class VIPClient : Person
+    {
+        public VIPClient(string firstName, string secondName, int age) : base(firstName, secondName, age, 1000000000)
+        { }
+
+        public override double[] CreateContribut()
+        {
+            int numberContributs = 0;
+
+            Console.WriteLine("Вы VIP клиент, на вклады у вас бесконечное количество денег");
+
+            Console.WriteLine("Меню создания вклада");
+            Console.Write("Введите количество вкладов: ");
+            numberContributs = int.Parse(Console.ReadLine());
+
+            while (numberContributs <= 0)
+            {
+                Console.WriteLine($"Введеное количество равно {numberContributs}\nЭто не допустимо! Введите новое количество вкладов превыщающий нуля");
+                numberContributs = int.Parse(Console.ReadLine());
+            }
+
+            double[] contributs = new double[numberContributs];
+
+            for (int i = 0; i < numberContributs; i++)
+            {
+                Console.Write($"Введите количество денег для {i + 1}: ");
+                double contribut = double.Parse(Console.ReadLine());
+
+                contributs[i] = contribut;
+            }
+
+            return contributs;
+        }
+    }
+}
